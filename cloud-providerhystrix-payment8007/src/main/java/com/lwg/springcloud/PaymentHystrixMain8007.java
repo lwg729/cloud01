f1acad8483bf9eb593dcf5ef89f714923a241dbc
lwg729 <1098372951@qq.com>
2020-11-02 17:14:58 +0800

    Hystrix搭建完成

--- cloud-providerhystrix-payment8007/src/main/java/com/lwg/springcloud/PaymentHystrixMain8007.java
+++ cloud-providerhystrix-payment8007/src/main/java/com/lwg/springcloud/PaymentHystrixMain8007.java
@@ -1,9 +1,12 @@
 package com.lwg.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +17,15 @@ public class PaymentHystrixMain8007 {
 
         SpringApplication.run(PaymentHystrixMain8007.class,args);
     }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 }
--- cloud-providerhystrix-payment8007/src/main/java/com/lwg/springcloud/service/PaymentService.java
+++ cloud-providerhystrix-payment8007/src/main/java/com/lwg/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.lwg.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,8 +16,8 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
     }
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandle",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandle", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
     })
     //失败
     public String paymentInfo_TimeOut(Integer id) {
@@ -27,10 +29,31 @@ public class PaymentService {
         }
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
     }
-    public String paymentInfo_TimeOutHandle(Integer id){
+
+    public String paymentInfo_TimeOutHandle(Integer id) {
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "服务降级托底";
     }
 
+
+    //==================服务熔断
+
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name="circuitBreaker.enabled",value = "true"),  //是否开启断路器
+            @HystrixProperty(name ="circuitBreaker.requestVolumeThreshold",value = "10"),  //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"), //时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60") //失败率达到多少后进行跳闸
+    })
+    public String payment_circuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("******id  不能为负数");
+        }
+        String serialNum = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t"+"调用成功,流水号"+serialNum;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能为负数，请稍后再试,此为服务熔断后的服务降级后的fallback方法  id: "+id;
+    }
 }
  
  
