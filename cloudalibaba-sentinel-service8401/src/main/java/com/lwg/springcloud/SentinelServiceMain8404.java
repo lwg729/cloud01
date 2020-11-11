03ac29de132b3df5bec8464299e68a40e3182659
lwg729 <1098372951@qq.com>
2020-11-11 18:26:21 +0800

    sentinel基础模块搭建完成

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lwg/springcloud/SentinelServiceMain8404.java
@@ -0,0 +1,13 @@
+package com.lwg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelServiceMain8404 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelServiceMain8404.class,args);
+    }
+}
